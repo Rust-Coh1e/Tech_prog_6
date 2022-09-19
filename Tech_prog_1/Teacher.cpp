e71f25d81f10786a1a4eb51743a7c4f52a8a2a95
Rust <Richard_jarvinen@ro.ru>
2022-09-19 11:46:35 +0300

    Add a Teacher class, except for virtual functions

--- Tech_prog_1/Teacher.cpp
+++ Tech_prog_1/Teacher.cpp
@@ -1,1 +1,39 @@
 #include "Teacher.h"
+
+using namespace std;
+
+Teacher::Teacher()
+{
+	cout << "Constructor Teacher without attributes was called." << endl;
+}
+
+Teacher::~Teacher()
+{
+	cout << "Destructor Teacher was called." << endl;
+}
+
+Teacher::Teacher(const Teacher& Teacher_copy)
+{
+	name = Teacher_copy.name;
+	groups = Teacher_copy.groups;
+	subs = Teacher_copy.subs;
+}
+
+Teacher::Teacher(string name)
+{
+	this->name = name;
+}
+
+string Teacher::get_name() const { return name; }
+
+void Teacher::set_name(string name_ch) { name = name_ch; }
+
+string Teacher::get_groups() const { return groups; }
+
+void Teacher::set_groups(string groups_ch) { groups = groups_ch; }
+
+string Teacher::get_subs() const { return subs; }
+
+void Teacher::set_subs(string subs_ch) { subs = subs_ch; }
+
+
--- Tech_prog_1/Teacher.h
+++ Tech_prog_1/Teacher.h
@@ -8,6 +8,13 @@ private:
     string groups;
     string subs;
 
+public:
+    Teacher();
+    ~Teacher();
+
+    Teacher(const Teacher& Teacher_copy);
+    Teacher(string name);
+
     string get_name() const;
     void set_name(string name_ch);
     string get_groups() const;
@@ -20,4 +27,3 @@ private:
     virtual void saving();
     virtual void redact_str(int num_str, string red_str);
 };
-
